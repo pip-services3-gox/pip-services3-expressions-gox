06c930f42f4f203e704b485bd9c17b61742fbd09
Dmitry Krayniy <krdima92@gmail.com>
2022-08-16 17:55:36 +0400

    Updated pipeline

--- test.ps1
+++ test.ps1
@@ -3,23 +3,24 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
-$testImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
+# Generate an image name using the data in the "$PSScriptRoot/component.json" file
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
+$testImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
 
 # Set environment variables
 $env:IMAGE = $testImage
 
 try {
     # Workaround to remove dangling images
-    docker-compose -f ./docker/docker-compose.test.yml down
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.test.yml" down
 
-    docker-compose -f ./docker/docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from test
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.test.yml" up --build --abort-on-container-exit --exit-code-from test
 
     # Save the result to avoid overwriting it with the "down" command below
     $exitCode = $LastExitCode 
 } finally {
     # Workaround to remove dangling images
-    docker-compose -f ./docker/docker-compose.test.yml down
+    docker-compose -f "$PSScriptRoot/docker/docker-compose.test.yml" down
 }
 
 # Return the exit code of the "docker-compose.test.yml up" command
